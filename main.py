d69c8c2563f3147aadf71d04393c411ee3e7f2e0
owwwl666 <zaharsalam228@gmail.com>
2025-05-19 22:39:42 +0900

    added star twinkle

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import curses
 
 def draw(canvas):
     row, column = (5, 20)
-    canvas.addstr(row, column, "Hello, World!")
     canvas.border()
-    canvas.refresh()
-    time.sleep(10)
+    while True: 
+        canvas.refresh()
+        canvas.addstr(row, column, "*",curses.A_DIM)
+        time.sleep(2)
+        canvas.refresh()
+        canvas.addstr(row, column, "*")
+        time.sleep(0.3)
+        canvas.refresh()
+        canvas.addstr(row, column, "*",curses.A_BOLD)
+        time.sleep(0.5)
+        canvas.refresh()
+        canvas.addstr(row, column, "*")
+        time.sleep(0.3)
 
 
 def main():
