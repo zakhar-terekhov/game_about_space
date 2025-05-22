a0667c71f6c34e3a82ee4ad00f4f2d670b350641
owwwl666 <zaharsalam228@gmail.com>
2025-05-22 22:44:55 +0900

    changed the name of the variables

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 async def blink(canvas, row, column, symbol):
     while True:
-        repeat = random.randint(1,10)
+        repeat = random.randint(1, 10)
         for _ in range(repeat):
             canvas.addstr(row, column, symbol, curses.A_DIM)
             await asyncio.sleep(0)
@@ -22,12 +22,12 @@ async def blink(canvas, row, column, symbol):
 
 
 def draw(canvas, amount=200):
-    height, width = curses.window.getmaxyx(canvas)
+    max_row, max_column = canvas.getmaxyx()
 
     coroutines = []
 
     for _ in range(amount):
-        row, column = random.randint(1, height - 1), random.randint(1, width - 1)
+        row, column = random.randint(1, max_row - 1), random.randint(1, max_column - 1)
         symbol = random.choice("+*.:")
         coroutines.append(blink(canvas, row, column, symbol))
 
